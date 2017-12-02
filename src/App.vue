adbce90c1296efd49a11c8054dd504e2b0e0631c
Kerminate <kepeilin@outlook.com>
2017-12-02 14:01:47 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+
+export default {
+  name: 'app',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="stylus">
+  body
+    margin: 0
+  #app
+    font-family: 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    text-align: center
+    color: #2c3e50
+</style>
