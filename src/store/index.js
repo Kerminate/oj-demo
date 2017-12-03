5503ecdabbea3ca9a424840e44e909d876d6459c
Kerminate <kepeilin@outlook.com>
2017-12-03 21:49:23 +0800

    modify vuex

--- src/store/index.js
+++ src/store/index.js
@@ -9,7 +9,7 @@ Vue.use(Vuex)
 // 初始化时用sessionStorage.getItem('token')，这样子刷新页面就无需重新登录
 const state = {
   token: window.sessionStorage.getItem('token'),
-  username: ''
+  username: window.sessionStorage.getItem('username')
 }
 
 export default new Vuex.Store({
