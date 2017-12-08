4dcf5c17df7d861260eebbaa07df13a029abba1a
lazzzis <shenlijin@outlook.com>
2017-12-07 21:46:07 -0500

    add logger and npm scripts

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const logger = require('koa-logger')
 const bodyparser = require('koa-bodyparser')
 require('./server/config/db.js')
 
 const app = new Koa()
 const router = new Router()
+
+// 日志，会在控制台显示请求的方法和路由
+app.use(logger())
 app.use(bodyparser())
 
 const session = require('./server/routes/session.js')
