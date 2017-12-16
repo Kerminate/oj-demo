5ee358e62f15cacfc988efd2b90ffea7bc2903f3
lazzzis <shenlijin@outlook.com>
2017-12-16 17:26:23 -0500

    try to improve the UI

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -1,26 +1,36 @@
 <template lang="html">
   <div class="user-wrap">
-    <ul>
-      <li>{{ user.uid }}</li>
-      <li><i class="el-icon-info"></i> Nick:&nbsp;&nbsp;{{ user.nick }}</li>
-      <li><i class="el-icon-edit"></i> Motto:&nbsp;&nbsp;{{ user.motto }}</li>
-      <li><i class="el-icon-message"></i> Mail:&nbsp;&nbsp;{{ user.mail }}</li>
-      <li><i class="el-icon-location-outline"></i> School:&nbsp;&nbsp;{{ user.school }}</li>
-      <li><i class="el-icon-success"></i> Solved:&nbsp;&nbsp;{{ user.solve }}</li>
-      <li><i class="el-icon-error"></i> Unsolved:&nbsp;&nbsp;{{ user.submit - user.solve }}</li>
-    </ul>
-    <div class="solved">
-      <div class="solved-name">Solved</div>
-      <el-button v-for="(item, index) in solved" :key="item" type="text">
-        <router-link :to="{ name: 'problemInfo', params: { pid: item } }">{{ item }}</router-link>
-      </el-button>
-    </div>
-    <div class="unsolved">
-      <div class="unsolved-name">Unolved</div>
-      <el-button v-for="item in unsolved" :key="item" type="text">
-        <router-link :to="{ name: 'problemInfo', params: { pid: item } }">{{ item }}</router-link>
-      </el-button>
-    </div>
+    <el-row>
+      <el-col :span="8">
+        <img src="../assets/logo.png" alt="">
+        <h1>{{ user.nick }}</h1>
+        <h3>{{ user.solve }} / {{ user.solve + user.submit }}</h3>
+        <i class="el-icon-info"></i> {{ `Nick: ${user.nick}` }}
+        <div class="motto">
+          <i class="el-icon-edit"></i> {{ `Motto: ${user.motto}` }}
+        </div>
+        <div v-if="user.mail">
+          <i class="el-icon-message"></i>{{ `Mail: ${user.mail}` }}
+        </div>
+        <div v-if="user.school">
+          <i class="el-icon-location-outline"></i>{{ `School: ${user.school}` }}
+        </div>
+      </el-col>
+      <el-col :span="16">
+        <div class="solved">
+          <div class="solved-name">Solved</div>
+          <el-button v-for="(item, index) in solved" :key="item" type="text">
+            <router-link :to="{ name: 'problemInfo', params: { pid: item } }">{{ item }}</router-link>
+          </el-button>
+        </div>
+        <div class="unsolved">
+          <div class="unsolved-name">Unolved</div>
+          <el-button v-for="item in unsolved" :key="item" type="text">
+            <router-link :to="{ name: 'problemInfo', params: { pid: item } }">{{ item }}</router-link>
+          </el-button>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -52,4 +62,9 @@ export default {
       padding: 0
     a
       color: #B12CCC
+    .image
+      width: 100%
+      display: block
+    .motto // motto 可能非常长，以至于一页放不下
+      word-break: break-all
 </style>
