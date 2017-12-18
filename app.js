3f8c0ddf47521299e075478720d2e631563628e9
lazzzis <shenlijin@outlook.com>
2017-12-18 13:10:38 -0500

    add a logger

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const Koa = require('koa')
-const logger = require('koa-logger')
+const koaLogger = require('koa-logger')
 const bodyparser = require('koa-bodyparser')
 const router = require('./server/routes')
+const logger = require('./server/utils/logger')
 require('./server/config/db.js')
 
 const app = new Koa()
 
 // 日志，会在控制台显示请求的方法和路由
-app.use(logger())
+app.use(koaLogger())
 
 app.use(bodyparser())
 
 app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(8888, () => {
-  console.log('The server is running at http://localhost:' + 8888)
+  logger.info('The server is running at http://localhost:' + 8888)
 })
