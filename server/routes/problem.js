791d789bc5e2a4d95a86e3b1884f354cf63fa64a
lazzzis <shenlijin@outlook.com>
2017-12-20 18:00:32 -0500

    rename

--- server/routes/problem.js
+++ server/routes/problem.js
@@ -1,12 +1,12 @@
 const problem = require('../controllers/problem')
 const Router = require('koa-router')
-const { loginRequired } = require('../utils/middlewares')
+const { auth } = require('../utils/middlewares')
 
 const router = new Router()
 
 router.get('/list', problem.list)
-router.get('/:pid', problem.preLoad, problem.findOne)
-router.post('/', loginRequired, problem.create)
-router.put('/:pid', loginRequired, problem.preLoad, problem.update)
+router.get('/:pid', problem.preload, problem.findOne)
+router.post('/', auth.login, auth.admin, problem.create)
+router.put('/:pid', auth.login, auth.admin, problem.preload, problem.update)
 
 module.exports = router
--- server/utils/middlewares.js
+++ server/utils/middlewares.js
@@ -1,8 +1,16 @@
-async function loginRequired (ctx, next) {
+async function login (ctx, next) {
   if (!ctx.session || ctx.session.user == null) ctx.throw(401, 'Login required')
   return next()
 }
 
+async function admin (ctx, next) {
+  // TODO 判断一个用户是不是 admin
+  return next()
+}
+
 module.exports = {
-  loginRequired
+  auth: {
+    login,
+    admin
+  }
 }
