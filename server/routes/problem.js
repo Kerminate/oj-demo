a7df2db28665d18c5b66226568b789d1b81e92c6
lazzzis <shenlijin@outlook.com>
2017-12-22 12:07:35 -0500

    remove session verify temporarily

--- server/routes/problem.js
+++ server/routes/problem.js
@@ -6,7 +6,7 @@ const router = new Router()
 
 router.get('/list', problem.list)
 router.get('/:pid', problem.preload, problem.findOne)
-router.post('/', auth.login, auth.admin, problem.create)
-router.put('/:pid', auth.login, auth.admin, problem.preload, problem.update)
+router.post('/', /*auth.login, auth.admin,*/ problem.create)
+router.put('/:pid', /*auth.login, auth.admin,*/ problem.preload, problem.update)
 
 module.exports = router
