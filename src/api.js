1ec519938dde4a1e35eee469f7ad7402a7e77d3d
lazzzis <shenlijin@outlook.com>
2017-12-22 16:15:17 -0500

    a proposal for api js

--- src/api.js
+++ src/api.js
@@ -5,11 +5,62 @@ import Vue from 'vue'
 axios.defaults.timeout = 5000 // 5000ms的超时验证
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8'
 
-// 创建一个axios实例
-const instance = axios.create()
-instance.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8'
+const instance = {}
 
-axios.interceptors.request.use = instance.interceptors.request.use
+// 对异常的基本处理
+['get', 'post', 'put', 'delete'].forEach((key) => {
+  instance[key] = function (...args) { // 闭包给原函数捕获异常
+    return axios[key](...args)
+      .catch((err) => {
+        if (err.response && err.response.status >= 500) {
+          Vue.prototype.$message({
+            message: `Σ(;ﾟдﾟ)  服务器崩坏，需要联系管理员维修`,
+            duration: 10000,
+            type: 'error',
+            showClose: true
+          })
+        } else if (err.response && err.response.status === 403) {
+          Vue.prototype.$message({
+            message: `╮(╯_╰)╭ 你没有相关权限进行此操作`,
+            duration: 10000,
+            type: 'warning',
+            showClose: true
+          })
+        } else if (err.response && err.response.status === 401) {
+          Vue.prototype.$message({
+            message: `(〃∀〃) 请先登录`,
+            duration: 10000,
+            showClose: true
+          })
+        } else if (err.response && err.response.status === 400) {
+          Vue.prototype.$message({
+            message: `${err.response.data.error}`,
+            duration: 10000,
+            showClose: true,
+            type: 'error'
+          })
+        } else if (!err.response) {
+          Vue.prototype.$message({
+            message: `_(:з」∠)_  网络异常，检查你的网线`,
+            duration: 10000,
+            type: 'error',
+            showClose: true
+          })
+        } else {
+          Vue.prototype.$message({
+            message: err.message,
+            duration: 10000,
+            type: 'error',
+            showClose: true
+          })
+        }
+        return Promise.reject(new Error('I throw this on purpose'))
+        // 继续抛出错误
+        // 不让后面的继续执行，也就是说，后面的 then 必然是在请求没有错误的情况下才执行的
+        // 因此后面不需要用 if (data.success) 语句判断是否有无错误
+      })
+  }
+})
 
 const api = {
   // 用户注册
@@ -70,62 +121,22 @@ const api = {
   // 更新题目
   updateProblem (data) {
     return instance.put(`/problem/${data.pid}`, data)
+  },
+  testdata: {
+    create: (data) => instance.post('...', data) // TODO
+  },
+  problem: {
+    findOne: (data) => instance.post('...', data),
+    find: (data) => instance.post('...', data),
+    create: (data) => instance.post('...', data),
+    update: (data) => instance.post('...', data),
   }
 }
 
-// 对异常的基本处理
-Object.entries(api).forEach(([key, value]) => {
-  api[key] = function (...args) { // 闭包给原函数捕获异常
-    return value(...args)
-      .catch((err) => {
-        if (err.response && err.response.status >= 500) {
-          Vue.prototype.$message({
-            message: `Σ(;ﾟдﾟ)  服务器崩坏，需要联系管理员维修`,
-            duration: 10000,
-            type: 'error',
-            showClose: true
-          })
-        } else if (err.response && err.response.status === 403) {
-          Vue.prototype.$message({
-            message: `╮(╯_╰)╭ 你没有相关权限进行此操作`,
-            duration: 10000,
-            type: 'warning',
-            showClose: true
-          })
-        } else if (err.response && err.response.status === 401) {
-          Vue.prototype.$message({
-            message: `(〃∀〃) 请先登录`,
-            duration: 10000,
-            showClose: true
-          })
-        } else if (err.response && err.response.status === 400) {
-          Vue.prototype.$message({
-            message: `${err.response.data.error}`,
-            duration: 10000,
-            showClose: true,
-            type: 'error'
-          })
-        } else if (!err.response) {
-          Vue.prototype.$message({
-            message: `_(:з」∠)_  网络异常，检查你的网线`,
-            duration: 10000,
-            type: 'error',
-            showClose: true
-          })
-        } else {
-          Vue.prototype.$message({
-            message: err.message,
-            duration: 10000,
-            type: 'error',
-            showClose: true
-          })
-        }
-        return Promise.reject(new Error('I throw this on purpose'))
-        // 继续抛出错误
-        // 不让后面的继续执行，也就是说，后面的 then 必然是在请求没有错误的情况下才执行的
-        // 因此后面不需要用 if (data.success) 语句判断是否有无错误
-      })
-  }
-})
+/**
+ * @example
+ * api.problem.findOne({ pid: 751 }).then((data) => console.log(data))
+ * api.status.find({ uid: 'admin' }).then((data) => console.log(data))
+ */
 
 export default api
